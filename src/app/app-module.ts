b96de322ff322729779ed04a2abc78ccce055460
Suhail Akhtar <suhailakhtar039@gmail.com>
2025-12-28 13:00:11 +0530

    proper layout created for parent

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,18 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { CoreModule } from './core/core-module';
+import { LayoutModule } from './layout/layout-module';
+import { SharedModule } from './shared/shared-module';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners()
-  ],
-  bootstrap: [App]
+  declarations: [App],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, LayoutModule, SharedModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
