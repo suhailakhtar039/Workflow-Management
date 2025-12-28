97a5ff00e057e3d8e027c95a33af51528b93e215
Suhail Akhtar <suhailakhtar039@gmail.com>
2025-12-28 13:29:59 +0530

    create proper structure

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -6,10 +6,26 @@ import { App } from './app';
 import { CoreModule } from './core/core-module';
 import { LayoutModule } from './layout/layout-module';
 import { SharedModule } from './shared/shared-module';
+import { AuthModule } from './features/auth/auth-module';
+import { DashboardModule } from './features/dashboard/dashboard-module';
+import { CasesModule } from './features/cases/cases-module';
+import { WorkflowModule } from './features/workflows/workflow-module';
+import { AdminModule } from './features/admin/admin-module';
 
 @NgModule({
   declarations: [App],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, LayoutModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    LayoutModule,
+    SharedModule,
+    AuthModule,
+    DashboardModule,
+    CasesModule,
+    WorkflowModule,
+    AdminModule,
+  ],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
---
+++ src/app/features/cases/cases-module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CasesRoutingModule } from './cases-routing-module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CasesRoutingModule
+  ]
+})
+export class CasesModule { }
