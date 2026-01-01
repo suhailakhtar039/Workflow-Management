88d21d9ea85b5fb25959d00352808fc6b7ab4226
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-01-01 16:22:33 +0530

    complete day 3

--- src/app/features/dashboard/dashboard-home/dashboard-home-component.ts
+++ src/app/features/dashboard/dashboard-home/dashboard-home-component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../core/auth/auth.service';
 
 @Component({
   selector: 'app-dashboard-home-component',
@@ -6,6 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard-home-component.html',
   styleUrl: './dashboard-home-component.css',
 })
-export class DashboardHomeComponent {
+export class DashboardHomeComponent implements OnInit {
+  username?: string;
+  role?: string;
+  isAdmin?: boolean;
+  isOfficer?: boolean;
+  isviewer?: boolean;
+  constructor(private authService: AuthService) {}
 
+  ngOnInit(): void {
+    this.authService.currentUser$.subscribe({
+      next: (user) => {
+        console.log('Current User:', user);
+        this.username = user?.username;
+        this.role = user?.role;
+        this.isAdmin = (user?.role as string) === 'ADMIN';
+        this.isOfficer = (user?.role as string) === 'OFFICER';
+        this.isviewer = (user?.role as string) === 'VIEWER';
+      },
+      error: (err) => {
+        console.error('Error fetching user:', err);
+      },
+    });
+  }
 }
