aa5686d88f55ca2ae8dc49ab9d6b14ad5bc23487
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-01-01 16:22:24 +0530

    use primeng module

--- src/app/features/dashboard/dashboard-module.ts
+++ src/app/features/dashboard/dashboard-module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing-module';
 import { DashboardHomeComponent } from './dashboard-home/dashboard-home-component';
+//primeng module
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
+// storing primeng modules and imports
+const primengModulesAndImports = [CardModule, ButtonModule];
 @NgModule({
   declarations: [DashboardHomeComponent],
-  imports: [CommonModule, DashboardRoutingModule],
+  imports: [CommonModule, DashboardRoutingModule, ...primengModulesAndImports],
 })
 export class DashboardModule {}
