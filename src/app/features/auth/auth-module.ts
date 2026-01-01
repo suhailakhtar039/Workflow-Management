1531dbe898d567abde5275c3f785c3092083e1a1
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-01-01 14:31:45 +0530

    import primeng modules

--- src/app/features/auth/auth-module.ts
+++ src/app/features/auth/auth-module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing-module';
 import { LoginComponent } from './login/login-component';
 
+//prime ng imports
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+//prime ng modules
+const primengModulesAndImports = [InputTextModule, PasswordModule, ButtonModule, CardModule];
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, AuthRoutingModule, FormsModule],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ...primengModulesAndImports,
+  ],
   exports: [LoginComponent],
 })
 export class AuthModule {}
