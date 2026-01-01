91b93d74a0cc2974f6a934995c2d93e344abfd8b
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-01-01 23:02:30 +0530

    importe primeng module in case module

--- src/app/features/cases/cases-module.ts
+++ src/app/features/cases/cases-module.ts
@@ -3,9 +3,25 @@ import { CommonModule } from '@angular/common';
 
 import { CasesRoutingModule } from './cases-routing-module';
 import { CaseListComponent } from './case-list/case-list-component';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputIconModule } from 'primeng/inputicon';
+import { IconFieldModule } from 'primeng/iconfield';
+import { FormsModule } from '@angular/forms';
 
+//prime ng imports
+
+//prime ng modules
+const primengModulesAndImports = [
+  ButtonModule,
+  TableModule,
+  InputTextModule,
+  InputIconModule,
+  IconFieldModule,
+];
 @NgModule({
   declarations: [CaseListComponent],
-  imports: [CommonModule, CasesRoutingModule],
+  imports: [CommonModule, FormsModule, CasesRoutingModule, ...primengModulesAndImports],
 })
 export class CasesModule {}
