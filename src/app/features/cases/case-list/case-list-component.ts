b116d3238ea595e105736ddb0d2cc14505fee611
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-01-01 23:02:54 +0530

    complete case layout

--- src/app/features/cases/case-list/case-list-component.ts
+++ src/app/features/cases/case-list/case-list-component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../core/auth/auth.service';
+import { Case, CaseService } from '../services/case-service';
+import { map, Observable } from 'rxjs';
+import { User } from '../../../core/auth/auth.model';
 
 @Component({
   selector: 'app-case-list-component',
@@ -7,5 +11,30 @@ import { Component } from '@angular/core';
   styleUrl: './case-list-component.css',
 })
 export class CaseListComponent {
+  currentUser$!: Observable<User | null>;
+  cases$!: Observable<Case[]>;
 
+  isAdmin$!: Observable<boolean>;
+  isOfficer$!: Observable<boolean>;
+  isViewer$!: Observable<boolean>;
+  dt: any;
+
+  constructor(private authService: AuthService, private caseService: CaseService) {
+    this.currentUser$ = this.authService.currentUser$;
+    this.cases$ = this.caseService.getCases();
+
+    this.isAdmin$ = this.currentUser$.pipe(map((user) => user?.role === 'ADMIN'));
+
+    this.isOfficer$ = this.currentUser$.pipe(map((user) => user?.role === 'OFFICER'));
+
+    this.isViewer$ = this.currentUser$.pipe(map((user) => user?.role === 'VIEWER'));
+  }
+
+  onEdit(caseItem: Case): void {
+    console.log('Edit case:', caseItem);
+  }
+
+  onDelete(caseItem: Case): void {
+    console.log('Delete case:', caseItem);
+  }
 }
