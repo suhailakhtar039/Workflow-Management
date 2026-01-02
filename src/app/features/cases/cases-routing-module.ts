17d81a07ed8f4289dae64bdbdc0e6827e4435476
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-01-02 19:26:42 +0530

    add new routing

--- src/app/features/cases/cases-routing-module.ts
+++ src/app/features/cases/cases-routing-module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CaseListComponent } from './case-list/case-list-component';
+import { CaseCreateComponent } from './case-create/case-create-component';
 
 const routes: Routes = [
   {
-    path:'',
-    component:CaseListComponent
-  }
+    path: '',
+    component: CaseListComponent,
+  },
+  {
+    path: 'create',
+    component: CaseCreateComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class CasesRoutingModule { }
+export class CasesRoutingModule {}
