ad59ce6df41a5ede5bd30b3051e4f26d78a70925
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-01-02 19:26:58 +0530

    create ractive form using primeflex

--- src/app/features/cases/case-create/case-create-component.ts
+++ src/app/features/cases/case-create/case-create-component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-case-create-component',
@@ -7,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './case-create-component.css',
 })
 export class CaseCreateComponent {
+  case!: FormGroup;
+  statusOptions;
+  constructor(private fb: FormBuilder) {
+    this.case = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(3)]],
+      description: [''],
+      assignedTo: ['', Validators.required],
+      status: ['', Validators.required],
+    });
+    this.statusOptions = [
+      { label: 'Low', value: 'Low' },
+      { label: 'Medium', value: 'Medium' },
+      { label: 'High', value: 'High' },
+    ];
+  }
 
+  onSubmit(): void {
+    console.log('Case Created:', this.case);
+  }
 }
