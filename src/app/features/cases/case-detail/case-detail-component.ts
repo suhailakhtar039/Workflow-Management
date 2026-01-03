deb09cdfc7dd8f04eba72c96313e72c3eb729eb2
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-01-03 15:41:17 +0530

    case detail ts file completed

--- src/app/features/cases/case-detail/case-detail-component.ts
+++ src/app/features/cases/case-detail/case-detail-component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Case, CaseService } from '../services/case-service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-case-detail-component',
@@ -7,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './case-detail-component.css',
 })
 export class CaseDetailComponent {
+  case!: Case;
+  id!: string;
+  found: boolean = true;
+  constructor(
+    private caseService: CaseService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.id = this.route.snapshot.paramMap.get('id')!;
+    this.case = this.caseService.getCaseById(this.id)!;
 
+    if (this.case === undefined) this.found = false;
+  }
+  goBack() {
+    this.router.navigate(['/cases']);
+  }
 }
