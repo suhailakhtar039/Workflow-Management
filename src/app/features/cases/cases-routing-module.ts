4306df7dcd1a227a2910dc033edd509e39efe99b
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-01-03 13:19:20 +0530

    add new path

--- src/app/features/cases/cases-routing-module.ts
+++ src/app/features/cases/cases-routing-module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CaseListComponent } from './case-list/case-list-component';
 import { CaseCreateComponent } from './case-create/case-create-component';
+import { CaseDetailComponent } from './case-detail/case-detail-component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'create',
     component: CaseCreateComponent,
+  },
+  {
+    path: ':id',
+    component: CaseDetailComponent,
   },
 ];
 
